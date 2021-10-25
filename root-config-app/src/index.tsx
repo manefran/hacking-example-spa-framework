c06da84cf300e228454ab4c5ce11fade1bb304d9
Manuel Arrieta Vallejo <marrieta.vallejo@gmail.com>
2021-10-24 19:19:57 -0500

    se realizan ajustes para que no aparecen en la guia

--- root-config-app/src/index.tsx
+++ root-config-app/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { LifeCycles, registerApplication, start } from "single-spa";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,6 +11,20 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+registerApplication({
+  name: "app-angular",
+  app: (): Promise<LifeCycles> => (window as any).System.import("http://localhost:4200/main.js"),
+  activeWhen: "/angular"
+});
+
+registerApplication({
+  name: "app-react",
+  app: (): Promise<LifeCycles> => (window as any).System.import("@single-spa-test/app-react"),
+  activeWhen: "/react"
+});
+
+start();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
